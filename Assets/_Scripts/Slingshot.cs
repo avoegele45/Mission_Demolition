1da95eac834052d957472ff7cf64284869d6ce24
avoegele45 <avoegel@siue.edu>
2025-03-09 08:38:03 -0500

    fixed audio and rubber band problems

--- Assets/_Scripts/Slingshot.cs
+++ Assets/_Scripts/Slingshot.cs
@@ -19,11 +19,12 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
+
     static public Vector3 LAUNCH_POS
     {
         get
         {
-            if(S == null) return Vector3.zero;
+            if (S == null) return Vector3.zero;
             return S.launchPos;
         }
     }
@@ -32,7 +33,7 @@ public class Slingshot : MonoBehaviour
     {
         S = this;
 
-        Transform launchPointTrans = transform.Find("LaunchPoint"); // a
+        Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
         launchPos = launchPointTrans.position;
@@ -40,61 +41,47 @@ public class Slingshot : MonoBehaviour
         rubberBand.enabled = false;
 
         audioSource = GetComponent<AudioSource>();
-
+        if (snapSound == null)
+            Debug.LogError("Snap sound is missing!");
     }
+
     void OnMouseEnter()
     {
         launchPoint.SetActive(true);
-        //print("Slingshot:OnMouseEnter()");
     }
 
     void OnMouseExit()
     {
         launchPoint.SetActive(false);
-        //print("Slingshot:OnMouseExit()");
     }
 
     private void OnMouseOver()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
             aimingMode = true;
-            projectile = Instantiate(prefabProjectile1) as GameObject;
+            GameObject selectedPrefab = Input.GetMouseButtonDown(0) ? prefabProjectile1 : prefabProjectile2;
+            projectile = Instantiate(selectedPrefab) as GameObject;
             projectile.transform.position = launchPos;
-            projectile.GetComponent<Rigidbody>().isKinematic = true;
 
             projectileRigidbody = projectile.GetComponent<Rigidbody>();
             projectileRigidbody.isKinematic = true;
             rubberBand.enabled = true;
-
         }
-        if (Input.GetMouseButtonDown(1))
-        {
-            aimingMode = true;
-            projectile = Instantiate(prefabProjectile2) as GameObject;
-            projectile.transform.position = launchPos;
-            projectile.GetComponent<Rigidbody>().isKinematic = true;
-
-            projectileRigidbody = projectile.GetComponent<Rigidbody>();
-            projectileRigidbody.isKinematic = true;
-            rubberBand.enabled = true;
-
-        }
-
     }
 
     private void Update()
     {
-        if(!aimingMode)
+        if (!aimingMode)
             return;
+
         Vector3 mousePos2d = Input.mousePosition;
         mousePos2d.z = -Camera.main.transform.position.z;
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2d);
 
         Vector3 mouseDelta = mousePos3D - launchPos;
         float maxMagnitude = this.GetComponent<SphereCollider>().radius;
-        if(mouseDelta.magnitude > maxMagnitude)
+        if (mouseDelta.magnitude > maxMagnitude)
         {
             mouseDelta.Normalize();
             mouseDelta *= maxMagnitude;
@@ -102,10 +89,9 @@ public class Slingshot : MonoBehaviour
 
         Vector3 projPos = launchPos + mouseDelta;
         projectile.transform.position = projPos;
+        UpdateRubberBand(projPos);
 
-        UpdateRubberBand(projPos); 
-    
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
         {
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
@@ -114,32 +100,16 @@ public class Slingshot : MonoBehaviour
             projectile = null;
             MissionDemolition.ShotFired();
             ProjectileLine.S.poi = projectile;
-            rubberBand.enabled = true;
 
+            PlaySnapSound();
+            StartCoroutine(DisableRubberBand()); // Delay disabling the rubber band
         }
-        if (Input.GetMouseButtonUp(1))
-        {
-            aimingMode = false;
-            projectileRigidbody.isKinematic = false;
-            projectileRigidbody.velocity = -mouseDelta * velocityMult;
-            FollowCam.POI = projectile;
-            projectile = null;
-            MissionDemolition.ShotFired();
-            ProjectileLine.S.poi = projectile;
-            rubberBand.enabled = true;
-
-        }
-
-        rubberBand.enabled = false;
-        PlaySnapSound();
-
     }
 
     private void UpdateRubberBand(Vector3 projPos)
     {
         rubberBand.SetPosition(0, launchPos);
         rubberBand.SetPosition(1, projPos);
-
     }
 
     private void PlaySnapSound()
@@ -150,5 +120,9 @@ public class Slingshot : MonoBehaviour
         }
     }
 
-
+    private IEnumerator DisableRubberBand()
+    {
+        yield return new WaitForSeconds(0.1f); // Small delay to let it be visible
+        rubberBand.enabled = false;
+    }
 }
